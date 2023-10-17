b2af808d972f0de7eebc5cf74c5faf47050d25c8
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2023-10-17 11:56:51 -0700

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,19 @@
+x = 1
+y = 2
+a = 3.0
+b = 4.0
+
+print("x = 1, y = 2, a = 3.0, b = 4.0")
+
+
+w = a + b
+print("a + b = ", w)
+print(w, " is a ", type(w))
+
+c = y - x
+print("y - x = ", c)
+print(c, " is a ", type(c))
+
+f = y * b
+print("y * b = ", f)
+print(f, " is a ", type(f))
